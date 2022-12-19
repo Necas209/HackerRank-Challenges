cfd79d0577fd34d74a3f0b2599553f6948e3ff94
Diogo Medeiros <diogo_medeiros@sapo.pt>
2022-12-19 02:52:59 +0000

    Update credit_card.py

--- Python/Regex and Parsing/credit_card.py
+++ Python/Regex and Parsing/credit_card.py
@@ -1,9 +1,15 @@
 import re
 
-if __name__ == '__main__':
+
+def main() -> None:
     n = int(input())
     for _ in range(n):
         cc_number = input()
-        repeats = re.match(R'.*(\d)\1{3,}.*', cc_number.replace('-', ''))
-        valid_cc = re.match(R'^[456]\d{3}(-?\d{4}){3}$', cc_number)
+        repeats = re.match(r'.*(\d)\1{3,}.*', cc_number.replace('-', ''))
+        valid_cc = re.match(r'^[456]\d{3}(-?\d{4}){3}$', cc_number)
         print('Valid' if not repeats and valid_cc else 'Invalid')
+
+
+if __name__ == '__main__':
+    main()
+    
