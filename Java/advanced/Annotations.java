ac78b680f6f0c0034ecb1c0cf5a7e0aa945387fe
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-01-25 16:28:49 +0000

    Updated Annotations.java

--- Java/advanced/Annotations.java
+++ Java/advanced/Annotations.java
@@ -34,6 +34,27 @@ class FamilyMember {
 }
 
 public class Annotations {
+    static void processAnnotations(String role, int spend) throws Exception {
+        Class<?> annotatedClass = FamilyMember.class;
+        Method[] methods = annotatedClass.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(FamilyBudget.class)) {
+                FamilyBudget family = method
+                        .getAnnotation(FamilyBudget.class);
+                String userRole = family.userRole();
+                int budgetLimit = family.budgetLimit();
+                if (userRole.equals(role)) {
+                    if (budgetLimit >= spend) {
+                        method.invoke(FamilyMember.class.getDeclaredConstructor().newInstance(),
+                                budgetLimit, spend);
+                    } else {
+                        System.out.println("Budget Limit Over");
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int testCases = Integer.parseInt(in.nextLine());
@@ -41,24 +62,7 @@ public class Annotations {
             String role = in.next();
             int spend = in.nextInt();
             try {
-                Class<?> annotatedClass = FamilyMember.class;
-                Method[] methods = annotatedClass.getMethods();
-                for (Method method : methods) {
-                    if (method.isAnnotationPresent(FamilyBudget.class)) {
-                        FamilyBudget family = method
-                                .getAnnotation(FamilyBudget.class);
-                        String userRole = family.userRole();
-                        int budgetLimit = family.budgetLimit();
-                        if (userRole.equals(role)) {
-                            if (budgetLimit >= spend) {
-                                method.invoke(FamilyMember.class.getDeclaredConstructor().newInstance(),
-                                        budgetLimit, spend);
-                            } else {
-                                System.out.println("Budget Limit Over");
-                            }
-                        }
-                    }
-                }
+                processAnnotations(role, spend);
             } catch (Exception e) {
                 e.printStackTrace();
             }
